832eb15f93cd66500c322ea4afd703e49c3bb689
Weltraumbiene <ben@benben.de>
2024-09-27 15:04:13 +0200

    Der Code wurde angepasst und scheint zu funktionieren

--- todo-api.php
+++ todo-api.php
@@ -2,19 +2,38 @@
 
 header("Content-Type: application/json");
 
+
+function write_log($action, $data) {
+    $log = fopen('log.txt', 'a');
+    $timestamp = date('Y-m-d H:i:s');
+    fwrite($log, "$timestamp - $action: " . json_encode($data) . "\n");
+    fclose($log);
+}
+
 switch ($_SERVER["REQUEST_METHOD"]) {
     case "GET":
-        // Get TODO (Read)
+        // Get Todo's (READ)
+        $todos = [
+            ["id" => "uniqID", "todo" => "First TODO"]
+        ];
+        echo json_encode($todos);
+        write_log("READ", $todos); 
         break;
     case "POST":
-        // ADD Todo (CREATE)
+        // Add Todo (CREATE)
+        $input = json_decode(file_get_contents('php://input'), true); 
+        write_log("CREATE", $input);
         break;
     case "PUT":
-        //Change Todo (UPDATE)
+        // Change Todo (UPDATE)
+        $input = json_decode(file_get_contents('php://input'), true); 
+        write_log("UPDATE", $input);
         break;
-        case "DELETE":
-            //REMOVE TODO DELETE
+    case "DELETE":
+        // Remove Todo (DELETE)
+        $input = json_decode(file_get_contents('php://input'), true); 
+        write_log("DELETE", $input);
         break;
-    
 }
+
 ?>
